e069135357477550ebe03d549107f6c561c0c4f3
Arrow <Arrow1604@mail.com>
2020-05-14 14:41:42 +0300

    Initial Commit

---
+++ ipad1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ipad1
+//
+//  Created by arrow on 5/14/20.
+//  Copyright © 2020 arrow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
